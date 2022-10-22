389f66f20ea626a1c052fe1b222555e57565395d
nerpoc42 <nerpoc42@gmail.com>
2022-10-22 11:52:44 +0300

    TwoItems solution

---
+++ TwoItems/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        TwoItems<String, Integer> items = new TwoItems<>("Test", 123);
+
+        System.out.println(items.getFirst());
+        System.out.println(items.getSecond());
+    }
+}
---
+++ TwoItems/src/TwoItems.java
@@ -0,0 +1,27 @@
+public class TwoItems<A, B> {
+    private A first;
+
+    public A getFirst() {
+        return first;
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
+
+    private B second;
+
+    public TwoItems(A firstItem, B secondItem) {
+        this.first = firstItem;
+        this.second = secondItem;
+    }
+
+}

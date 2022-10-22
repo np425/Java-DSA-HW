a232661bac178201eb694745dc1f8f29b938148e
nerpoc42 <nerpoc42@gmail.com>
2022-10-22 10:32:22 +0300

    Updated stack

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,34 +1,48 @@
 public class Stack<T> {
-    private StackNode<T> lastNode;
+    private class Node {
+        T value;
+        Node prev;
+
+        Node(T value) {
+            this.value = value;
+        }
+
+        Node nth(int n) {
+            Node node = this;
+            int i = 0;
+
+            while (i < n && node != null) {
+                node = node.prev;
+                ++i;
+            }
+
+            return node;
+        }
+
+        Node findNode(T value) {
+            Node node = this;
+
+            while (node != null && node.value != value) {
+                node = node.prev;
+            }
+
+            return node;
+        }
+    }
+
+    private Node lastNode = null;
     private int length = 0;
 
     public void add(T value) {
-        if (this.lastNode == null) {
-            this.lastNode = new StackNode<>(value);
-        } else {
-            StackNode<T> node = new StackNode<>(value);
-            node.setPrev(this.lastNode);
+        if (this.lastNode != null) {
+            Node node = new Node(value);
+            node.prev = this.lastNode;
             this.lastNode = node;
+        } else {
+            this.lastNode = new Node(value);
         }
 
         ++this.length;
-    }
-
-    public T peek() {
-        return this.lastNode == null ? null : this.lastNode.getValue();
-    }
-
-    public boolean contains(T value) {
-        if (this.lastNode == null) {
-            return false;
-        }
-
-        StackNode<T> node = this.lastNode.findNode(value);
-        return node != null && node.getValue() == value;
-    }
-
-    public T get(int idx) {
-        return idx >= this.length ? null : this.lastNode.nth(idx).getValue();
     }
 
     public void remove(int idx) {
@@ -36,17 +50,33 @@ public class Stack<T> {
             return;
         }
 
-        if (idx == 0) {
+        if (idx != 0) {
+            Node priorNode = this.lastNode.nth(idx - 1);
+            Node targetNode = priorNode.prev;
 
-            this.lastNode = this.lastNode.getPrev();
+            priorNode.prev = targetNode.prev;
         } else {
-            StackNode<T> priorNode = this.lastNode.nth(idx - 1);
-
-            StackNode<T> targetNode = priorNode.getPrev();
-
-            priorNode.setPrev(targetNode.getPrev());
+            this.lastNode = this.lastNode.prev;
         }
 
         --this.length;
     }
+
+    public T peek() {
+        return this.lastNode == null ? null : this.lastNode.value;
+    }
+
+    public boolean contains(T value) {
+        if (this.lastNode == null) {
+            return false;
+        }
+
+        Node node = this.lastNode.findNode(value);
+        return node != null && node.value == value;
+    }
+
+    public T get(int idx) {
+        return idx >= this.length ? null : this.lastNode.nth(idx).value;
+    }
+
 }
